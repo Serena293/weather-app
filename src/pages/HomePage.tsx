a70b4c8b1f905a96aeae20fe375601e069d4f1f3
SERENA FERRARO <serenaferraro21@gmail.com>
2025-06-29 16:00:28 +0100

    layout HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -5,11 +5,7 @@ import Footer from "../components/Footer";
 import SearchBar from "../components/Searchbar";
 import WeatherCard from "../components/WeatherCard";
 import type { CityOption } from "../types";
-import {
-  WEATHER_API_URL,
-  FORECAST_URL,
-  WEATHER_API_KEY,
-} from "../api"; 
+import { WEATHER_API_URL, FORECAST_URL, WEATHER_API_KEY } from "../api";
 
 const HomePage = () => {
   const [weatherData, setWeatherData] = useState(null);
@@ -21,7 +17,6 @@ const HomePage = () => {
     const [lat, lon] = city.value.split(" ");
 
     try {
- 
       const currentRes = await axios.get(WEATHER_API_URL, {
         params: {
           lat,
@@ -52,10 +47,24 @@ const HomePage = () => {
 
   return (
     <>
-      <Navbar />
-      <SearchBar onSearch={handleSearch} />
-      <WeatherCard data={weatherData} forecast={forecastData} />
-      <Footer />
+      <div className="app-wrapper d-flex flex-column min-vh-100">
+        <Navbar />
+        <div className="my-2 p-4">
+          <SearchBar onSearch={handleSearch} />
+        </div>
+
+        <main className="flex-grow-1 d-flex justify-content-center align-items-center">
+          {!weatherData ? (
+            <p className="fs-4 text-secondary">
+              Select a city to see the weather 🌤️
+            </p>
+          ) : (
+            <WeatherCard data={weatherData} forecast={forecastData} />
+          )}
+        </main>
+
+        <Footer />
+      </div>
     </>
   );
 };
